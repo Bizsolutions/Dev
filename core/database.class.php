ec9f1e7a36199a1f01743f7a3e10cde196364a4d
indrakumarjatav <indrakumarjatav@gmail.com>
2020-08-28 19:57:25 +0530

    chagne db file

--- core/database.class.php
+++ core/database.class.php
@@ -1,65 +1,61 @@
 <?php
 
-
 //database class 
 class db_class {
-	
-	
-	//init the time zone setting, should run as soon as the class is instanced
-	var $db_server; // database server
-	var $db_username; // database username
-	var $db_password;	// database password
-	var $db_name; // database name
-	
-	
-	function __construct()
-    {
+
+    //init the time zone setting, should run as soon as the class is instanced
+    var $db_server; // database server
+    var $db_username; // database username
+    var $db_password; // database password
+    var $db_name; // database name
+
+    function __construct() {
         $this->db_server = "localhost";
-	//Local server
-	/*	
-		$this->db_username = "root";
-		$this->db_password = "admin";
-		$this->db_name = "topmoversreview";*/
-		
-		
-		// Arvix Server
-	/*	$this->db_name = "bishara7_topmovingreview";
-		$this->db_username = "bishara7_review";
-		$this->db_password = "CV&[wi#1ceK)";*/
-	
-						// Arvix Server
+        //Local server
+        /* 	
+          $this->db_username = "root";
+          $this->db_password = "admin";
+          $this->db_name = "topmoversreview"; */
 
-		$this->db_name = "topmovin_mymovingreviews";
 
-		$this->db_username = "topmovin_mmr";
+        // Arvix Server
+        /* 	$this->db_name = "bishara7_topmovingreview";
+          $this->db_username = "bishara7_review";
+          $this->db_password = "CV&[wi#1ceK)"; */
 
-		$this->db_password = "m&[^y71j*a@&";
-		
+        // Arvix Server
+//		$this->db_name = "topmovin_mymovingreviews";
+//
+//		$this->db_username = "topmovin_mmr";
+//
+//		$this->db_password = "m&[^y71j*a@&";
+        $this->db_name = "topmovin_mymovingreviews";
+
+        $this->db_username = "root";
+
+        $this->db_password = "";
     }
-	
-	
-	//connect function
-	function db_connect() {
-		global $link;
-		 $link = mysqli_connect($this->db_server, $this->db_username, $this->db_password); 
-		if($link) { 
-   			mysqli_select_db($link,$this->db_name);
-		}
-		return $link;	
-	}
-	
-	//disconnect from database
-	function db_disconnect($link) {
-		
-		if($link) { 
-			mysqli_close($link);
-		}
-		
-	}
-		
+
+    //connect function
+    function db_connect() {
+        global $link;
+        $link = mysqli_connect($this->db_server, $this->db_username, $this->db_password);
+        if ($link) {
+            mysqli_select_db($link, $this->db_name);
+        }
+        return $link;
+    }
+
+    //disconnect from database
+    function db_disconnect($link) {
+
+        if ($link) {
+            mysqli_close($link);
+        }
+    }
+
 }
+
 $connection = new db_class(); //i created a new object
 $connection->db_connect();
-
-
 ?>
